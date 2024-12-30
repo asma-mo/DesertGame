535bdd963f241f6869b5e34353b297908f7473f3
asma <Asma.mo@outlook.com>
2024-12-30 19:30:04 +0300

    Initial Commit

---
+++ DesertGame/DesertGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesertGameApp.swift
+//  DesertGame
+//
+//  Created by Asma on 30/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct DesertGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
