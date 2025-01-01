053f75075e395f6b5bef1617f61ff8f4ab8ca8ca
asma <Asma.mo@outlook.com>
2025-01-01 14:50:13 +0300

    Add onboarding

--- DesertGame/App/Views/SplashScreen.swift
+++ DesertGame/App/Views/SplashScreen.swift
@@ -1,10 +1,9 @@
-
 import SwiftUI
 
 struct SplashScreen: View {
     @EnvironmentObject var viewModel: GameViewModel
     @State private var showStartingPage: Bool = false
-
+    @State private var showslideShow: Bool = false
     @State private var opacity = 0.0
 
     var body: some View {
@@ -12,25 +11,24 @@ struct SplashScreen: View {
             ZStack {
                 Color.black.edgesIgnoringSafeArea(.all)
                 
-                if showStartingPage {
-                    StartingPage().environmentObject(viewModel)
-                } else if !showStartingPage{
+                if showslideShow {
+                                  SlidingPagesView()
+                              } else if showStartingPage {
+                                  StartingPage().environmentObject(viewModel)
+                              } else {
                     Image("logo")
                         .resizable()
                         .scaledToFit()
                         .frame(width: 1000, height: 1000)
-                        .opacity(opacity)
                         .padding()
-                        .accessibilityElement()
-                        .accessibilityLabel("splash screen")
-                        .accessibilityHint("person in a desert and SAND SOUND glowing")
                         .onAppear {
                             withAnimation(.easeInOut(duration: 1.5)) {
                                 opacity = 1.0
                             }
                             DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
                                 withAnimation {
-                                    showStartingPage = true
+                        showStartingPage = true
+                        showslideShow = true
                                 }
                             }
                         }
--- DesertGame/App/Views/slideShow.swift
+++ DesertGame/App/Views/slideShow.swift
@@ -7,12 +7,55 @@
 
 import SwiftUI
 
-struct slideShow: View {
+
+struct SlidingPagesView: View {
+    @State private var showStartingPage: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        if showStartingPage {
+            StartingPage()
+        } else {
+            TabView {
+                PageView(content: "ثبت الهاتف عموديًا!")
+                    .accessibilityLabel("ثبت الهاتف عموديًا!")
+                    .background(Color.black)
+                    .tag(0)
+                
+                PageView(content: "دع انتباهك علي تفاصيل الاصوات!")
+                    .accessibilityLabel("دع انتباهك على تفاصيل الاصوات!")
+                    .background(Color.black)
+                    .tag(1)
+            }
+            .tabViewStyle(PageTabViewStyle())
+            .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+            .ignoresSafeArea()
+            .onAppear {
+                
+                DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+                    showStartingPage = true
+                }
+            }
+        }
     }
 }
 
-#Preview {
-    slideShow()
+struct PageView: View {
+    var content: String
+    
+    var body: some View {
+        VStack {
+            Text(content)
+                .font(.largeTitle)
+                .foregroundColor(.white)
+                .padding()
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct SlidingPagesView_Previews: PreviewProvider {
+    static var previews: some View {
+        SlidingPagesView()
+    }
 }
